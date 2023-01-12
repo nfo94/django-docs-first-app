382947970b7f47c07c14a05b2b5c3a5772192d0f
Natalia Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-01-12 07:47:15 -0300

    add part 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,25 +1,46 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
 
 # Create your views here.
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id) -> render:
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id) -> HttpResponse:
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id) -> HttpResponse:
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST is a dictionary-like object that lets you access submitted data by key name
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls.detail.html",
+            {"question": question, "error_message": "You didn't select a choice"},
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # the reverse() function helps avoid having to hardcode a URL in the view function
+        # it will return a string like '/polls/3/results/'
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
